e6c1b61f4b01101bf9f5896ed140b6b1a705c86c
suzan-naeem <64129170+suzan-naeem@users.noreply.github.com>
2021-06-08 03:06:39 +0200

    Update BooksList.js

--- src/BooksList.js
+++ src/BooksList.js
@@ -1,17 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext , useState } from "react";
 import { booksContext } from "./contexts/booksContext";
+import ItemsCarousel from 'react-items-carousel';
+
 
 import { BrowserRouter , Link } from "react-router-dom";
 
 function ChildComponent() {
   const { books, setBooks } = useContext(booksContext);
+  const [activeItemIndex, setActiveItemIndex] = useState(0);
+  const chevronWidth = 40;
   return (
-    <div
-      style={{ textAlign: "left", display: "flex" }}
+    <div style={{ padding: `0 ${chevronWidth}px` }}>
+    <ItemsCarousel
+      requestToChangeActive={setActiveItemIndex}
+      activeItemIndex={activeItemIndex}
+      numberOfCards={4}
+      gutter={20}
+      leftChevron={<button>{'<'}</button>}
+      rightChevron={<button>{'>'}</button>}
+      outsideChevron
+      chevronWidth={chevronWidth}
     >
+    <div
+      style={{ textAlign: "left", display: "flex"}}
+    > 
+      
       {books.map((book, index) => (
-        <div  key={book.id} style={{ display: "flex", justifyContent: "space-between" }}>
-          <div><img src={book.volumeInfo.imageLinks.thumbnail} style={{ display: 'block' }} alt="" /></div>
+        <div  key={book.id} style={{ display: "flex", justifyContent: "space-around" , flexDirection: "column" , marginLeft: '20px'}}>
+          <div><img src={book.volumeInfo.imageLinks.thumbnail} style={{ width: '150px',height: '200px' }} alt="" /></div>
           <div >- {book.volumeInfo.title}</div>
           <BrowserRouter>
           <Link to={`/BookDetails/${book.id}`} style={{ textDecoration: 'none' }}>
@@ -20,6 +36,9 @@ function ChildComponent() {
           </BrowserRouter>
         </div>
       ))}
+      
+    </div>
+    </ItemsCarousel>
     </div>
   );
 }
